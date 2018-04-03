e27987b5451da63830ca7ab6ae4eff034ad06b39
Jonathan Holland <jonathan.holland8@gmail.com>
2018-04-03 20:45:20 +1000

    Actually rendering the list view
    - tested by adding a string to the adapter directly
    - the next step would be to adjust the contents to take the rosteredEvent object information for display

--- app/src/main/java/holland/jonathan/nurserostering/MainActivity.java
+++ app/src/main/java/holland/jonathan/nurserostering/MainActivity.java
@@ -3,23 +3,51 @@ package holland.jonathan.nurserostering;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     CalendarView calendar;
-    ListView rosteredEvents;
+    ArrayList<String> listItems = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         calendar = (CalendarView) this.findViewById(R.id.calendarView);
-        rosteredEvents = (ListView) this.findViewById(R.id.rosteredEventsView);
+        listView = (ListView) this.findViewById(R.id.rosteredEventView);
+
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        setListAdapter(adapter);
     }
 
     @Override
     public void onClick(View v) {
 
+    }
+
+    protected ListView getListView() {
+        if (this.listView == null) {
+            this.listView = (ListView) findViewById(R.id.rosteredEventView);
+        }
+        return this.listView;
+    }
+
+    protected void setListAdapter(ListAdapter adapter) {
+        getListView().setAdapter(adapter);
+    }
+
+    protected ListAdapter getListAdapter() {
+        ListAdapter adapter = getListView().getAdapter();
+        return adapter;
     }
 }
