d3fd3197cd55cf9439a604346f4a4552ac309427
Jonathan Holland <jonathan.holland8@gmail.com>
2018-08-27 19:26:09 +1000

    Increase size of decorator circle to make it more noticeable

--- app/src/main/java/holland/jonathan/nurserostering/DateColourDecorator.java
+++ app/src/main/java/holland/jonathan/nurserostering/DateColourDecorator.java
@@ -25,6 +25,6 @@ public class DateColourDecorator implements DayViewDecorator {
 
     @Override
     public void decorate(DayViewFacade view) {
-        view.addSpan(new DotSpan(5, color));
+        view.addSpan(new DotSpan(50, color));
     }
 }
